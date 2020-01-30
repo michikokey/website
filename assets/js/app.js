6fa9f158c74cb002dba956072b17c286079239a6
michikokey <m19510818@gmail.com>
2020-01-30 10:04:53 +0900

    world enjoy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-
+// 
 
 $('#b').on('mouseover', function() {
     $(this).text('運動');
@@ -7,3 +7,38 @@ $('#b').on('mouseover', function() {
 $('#b').on('mouseleave', function() {
     $(this).text('Be Athlete');
 });
+
+// 
+$('#g').on('mouseover', function() {
+    $(this).text('世界へ！');
+});
+
+$('#g').on('mouseleave', function() {
+    $(this).text('go world');
+});
+
+// 
+$('#e').on('mouseover', function() {
+    $(this).text('楽しもう！');
+});
+
+$('#e').on('mouseleave', function() {
+    $(this).text('Enjoy Everything');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
